dcc05c2eabe1b8c942761f7f0d96a7cf207cc24f
Sergio Bastos Jr <sergiobastosfisio@gmail.com>
2024-12-26 18:01:16 -0300

    new upload

--- server/src/controllers/upload.ts
+++ server/src/controllers/upload.ts
@@ -1,44 +1,50 @@
 import { Request, Response } from "express";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import streamifier from "streamifier";
+import { PrismaClient } from "@prisma/client";
 
-import multer from "multer";
+const prisma = new PrismaClient();
 
-const aws = require("aws-sdk");
-
-const endpoint = new aws.Endpoint(process.env.BACKBLAZE_ENDPOINT_S3);
-const s3 = new aws.S3({
-  endpoint,
-  credentials: {
-    accessKeyId: process.env.BACKBLAZE_KEYID,
-    secretAccessKey: process.env.BACKBLAZE_APPLICATIONKEY,
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadImg = async (req: Request, res: Response) => {
-  const file: any = req.file;
+  console.log({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({ error: "Nenhum ficheiro foi enviado." });
+  }
 
   try {
-    const upload = await s3
-      .upload({
-        Bucket: process.env.BACKBLAZE_BUCKET,
-        Key: `${file.originalname}`,
-        Body: file.buffer,
-        ContentType: file.mimetype,
-      })
-      .promise();
-    const location = upload.Location;
+    const uploadToCloudinary = (fileBuffer: Buffer) => {
+      return new Promise<UploadApiResponse>((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          (error: any, result: any) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          }
+        );
+        streamifier.createReadStream(fileBuffer).pipe(uploadStream);
+      });
+    };
+    const result = await uploadToCloudinary(file.buffer);
+    return res.status(201).json({ url: result.secure_url, fileExist: false });
+  } catch (err) {
+    console.error("Erro ao fazer upload para o Cloudinary:", err);
 
-    if (!location.includes("https://") && !location.includes("http://")) {
-      upload.Location = `https://f005.backblazeb2.com/file${location}`;
-    }
-
-    return res.status(201).json({ file: upload });
-  } catch (error: any) {
-    console.log(error);
-
-    return res
-      .status(error.status || 500)
-      .json({ error: error.message || "Erro ao fazer o upload do arquivo" });
+    return res.status(500).json({
+      error: "Erro ao fazer o upload do ficheiro para o Cloudinary",
+    });
   }
 };
-
-export default uploadImg;
